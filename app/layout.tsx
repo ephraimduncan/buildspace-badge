d596b5611f2ef9920630ddecdafe2ee5fe3b4f47
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-18 07:55:08 +0000

    aesthetics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import { Toaster } from "sonner";
 import "./globals.css";
 import { Manrope } from "next/font/google";
+import { Metadata } from "next";
 
 const font = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
 });
+
+export const metatada: Metadata = {
+  title: "n&w badge",
+  description: "Select your house and download your badge",
+};
 
 export default function RootLayout({
   children,
